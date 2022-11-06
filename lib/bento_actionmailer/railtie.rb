c164dac14b5378cadfccf1ce36d2a48d090a525b
Sebastian Szturo <sebastian.szturo@gmail.com>
2022-11-06 15:05:08 +0900

    rubocop -a

--- lib/bento_actionmailer/railtie.rb
+++ lib/bento_actionmailer/railtie.rb
@@ -1,6 +1,6 @@
 module BentoActionMailer
   class Railtie < Rails::Railtie
-    initializer 'bento_action_mailer.add_delivery_method', before: 'action_mailer.set_configs' do
+    initializer "bento_action_mailer.add_delivery_method", before: "action_mailer.set_configs" do
       ActiveSupport.on_load(:action_mailer) do
         ActionMailer::Base.add_delivery_method(:bento_actionmailer, BentoActionMailer::DeliveryMethod)
       end
